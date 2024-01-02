91e98a87ffb1e5d682af67cd0067df63298e48ec
JenniferSchroeder <152738686+JenniferSchroeder@users.noreply.github.com>
2024-01-02 13:51:50 -0600

    added jquery and hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,12 @@ var person = {
     hobbies: "camping, arting, music",
   };
 
-console.log(person)
-for (var i = 1; i <= 100; i++) {
-    console.log(i);
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
   }

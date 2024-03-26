c33a2ed088764d799f50b99cd4e88e6430a5a563
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-03-25 20:23:49 -0700

    carousel in working condition

---
+++ js/swiper-carousel.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.mySwiper', {
+	cssMode: true,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	mousewheel: true,
+	keyboard: true,
+});

cc13a0a0c3d4a5d16df4b3d42925a7efd749f998
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-03-25 20:41:01 -0700

    Added auto change to swiper

--- js/swiper-carousel.js
+++ js/swiper-carousel.js
@@ -1,6 +1,9 @@
 var swiper = new Swiper('.mySwiper', {
 	cssMode: true,
 	loop: true,
+	autoplay: {
+		delay: 8000,
+	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
